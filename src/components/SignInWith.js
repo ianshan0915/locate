8d7ae7b962bf39b1a28fb2a23c48363cf40df78b
ian Shen <ianshan0915@gmail.com>
2018-09-25 12:26:44 +0200

    add google auth client_id

--- src/components/SignInWith.js
+++ src/components/SignInWith.js
@@ -36,7 +36,7 @@ class SignInWith extends Component {
 
                 <li className="omniauth-button google">
                     <GoogleLogin className="button google"
-                    clientId="" //802388286497-inltm6lgdpio24j9vuf2htdsbkfu0kg6.apps.googleusercontent.com
+                    clientId="802388286497-inltm6lgdpio24j9vuf2htdsbkfu0kg6.apps.googleusercontent.com"
                     onSuccess={responseGoogle}
                     onFailure={responseGoogle} >
                         <i className="fa fa-google"></i><span> SignIn with Google</span>
