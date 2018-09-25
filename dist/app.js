5e78a32ff9da75f25acf99638d8176be0fa9afc3
ian Shen <ianshan0915@gmail.com>
2018-09-25 12:14:02 +0200

    prepare for Heroku

--- dist/app.js
+++ dist/app.js
@@ -6,28 +6,28 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const path = require('path')
-const cloudinary = require('cloudinary')
+// const cloudinary = require('cloudinary')
 
 const app = express()
 const router = express.Router()
 //const url = process.env.MONGODB_URI || "mongodb://localhost:27017/medium"
-const url ="mongodb://alc:alc@ds237855.mlab.com:37855/alc"
+// const url ="mongodb://alc:alc@ds237855.mlab.com:37855/alc"
 
-/** configure cloudinary */
-cloudinary.config({
-    cloud_name: 'chidumennamdi',
-    api_key: '392481138676646',
-    api_secret: '6vN978wHnfEr21pKrysWuu7_0UI'
-})
+// /** configure cloudinary */
+// cloudinary.config({
+//     cloud_name: 'chidumennamdi',
+//     api_key: '392481138676646',
+//     api_secret: '6vN978wHnfEr21pKrysWuu7_0UI'
+// })
 
-/** connect to MongoDB datastore */
-try {
-    mongoose.connect(url, {
-        //useMongoClient: true
-    })    
-} catch (error) {
+// /** connect to MongoDB datastore */
+// try {
+//     mongoose.connect(url, {
+//         //useMongoClient: true
+//     })    
+// } catch (error) {
     
-}
+// }
 
 let port = process.env.PORT || 5000
 
@@ -45,7 +45,7 @@ app.use('/static',express.static(path.join(__dirname,'static')))
 app.use('/uploads',express.static(path.join(__dirname,'uploads')))
 app.use('/assets',express.static(path.join(__dirname,'assets')))
 
-app.use('/api', router)
+// app.use('/api', router)
 app.get('*', function(req, res) {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
