334b80725f5c31f957aaa53c35a18499dd40fd26
ian Shen <ianshan0915@gmail.com>
2018-10-29 14:35:09 +0800

    seperate backend and frontend

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 /** require dependencies */
 const express = require("express")
+const https = require('https')
 const routes = require('./routes/')
 const mongoose = require('mongoose')
 const cors = require('cors')
